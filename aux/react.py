125e36cdc8b714b82fbc015e4c8da1de5f4acee0
joao <joao@localhost>
2010-08-02 17:23:01 +0100

    Adding auxiliary files to parse trace files

---
+++ aux/react.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+
+#
+# Takes the output of a reflex load balancer and generates gnuplot files
+# for each network / interface.
+#
+# pipe through shell for plot
+# ./route.py file | sh
+
+
+
+import sys
+from os import system, remove
+
+if (len(sys.argv) != 2):
+  print "No args: route.py filename"
+  exit(0)
+
+try:
+  fdesc = open(sys.argv[1])
+except IOError:
+  print "File open error"
+  exit(0)
+
+networks = {}
+
+for line in fdesc:
+  elements = line.strip().split(" ");
+  
+  if elements[0] not in networks:
+    networks[elements[0]] = {}
+
+  if elements[1] not in networks[elements[0]]:
+    networks[elements[0]][elements[1]] = []
+
+  networks[elements[0]][elements[1]].append([elements[2], elements[3], elements[4]])
+
+#this creates list for each network/port pair
+for network, portlist in networks.iteritems():
+  for port, list in portlist.iteritems():
+    newfile = open ("."+network+"-"+port+".tr", 'w')
+    for entry in list:
+      newfile.write( str(entry[0])+" "+str(entry[1])+" "+str(entry[2])+"\n")
+    newfile.close()
+
+#now we need to generate the gnuplot files
+fwnd_plot = "u ($1/1000):2 with lines lw 3 t 'Fwnd int"
+loss_plot = "u ($1/1000):3 with lines lw 3 t 'Loss int"
+
+labels = "set ylabel 'Ratio'\nset xlabel 'Time(sec)'\n"
+
+replot = False
+
+for network, portlist in networks.iteritems():
+  fwndfile = open ("."+network+"-fwnd.gnu", 'w')
+  lossfile = open ("."+network+"-loss.gnu", 'w')
+  plotfwnd  = "gnuplot -persist ."+network+"-fwnd.gnu"
+  plotloss  = "gnuplot -persist ."+network+"-loss.gnu"
+  print plotfwnd
+  print plotloss
+  for port, list in portlist.iteritems():
+    if(replot):
+      fwndfile.write("replot '."+network+"-"+port+".tr' "+fwnd_plot+port+"'\n")
+      lossfile.write("replot '."+network+"-"+port+".tr' "+loss_plot+port+"'\n")
+    else:
+      fwndfile.write("set title '"+network+" fwnd graph'\n")
+      fwndfile.write(labels)
+      fwndfile.write("plot '."+network+"-"+port+".tr' "+fwnd_plot+port+"'\n")
+
+      lossfile.write("set title '"+network+" loss graph'\n")
+      lossfile.write(labels)
+      lossfile.write("plot '."+network+"-"+port+".tr' "+loss_plot+port+"'\n")
+      replot = True
+
